bb6267150d32fc61bd57f2c19b5926ca0dd33bf8
Thue Htet <thuehtetpyaee@gmail.com>
2024-11-19 23:48:49 +0630

    "Changed some error"

--- src/Game.kt
+++ src/Game.kt
@@ -1,42 +1,42 @@
 fun main(args: Array<String>) {
-        val options = arrayOf("Rock","Paper","Scissors")
-        val gameChoice = getGameChoice(options)
-        val userChoice = getUserChoice(options)
-        printResult(gameChoice,userChoice)
-    }
+    val options = arrayOf("Rock","Paper","Scissors")
+    val gameChoice = getGameChoice(options)
+    val userChoice = getUserChoice(options)
+    printResult(userChoice,gameChoice)
+}
 
-    fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random()*optionsParam.size).toInt()]
+fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random()*optionsParam.size).toInt()]
 
-    fun getUserChoice(optionsParam: Array<String>): String {
-        var isValidChoice = false
-        var userChoice = ""
-        while (!isValidChoice) {
-            print("Enter one of the following:")
-            for (item in optionsParam) print(" $item")
-            println(".")
-            val userInput = readLine()
-            if(userInput != null && userInput in optionsParam) {
-                isValidChoice = true
-                userChoice = userInput
-            }
-            if(!isValidChoice) {
-                println("You must enter a valid choice!")
-            }
+fun getUserChoice(optionsParam: Array<String>): String {
+    var isValidChoice = false
+    var userChoice = ""
+    while (!isValidChoice) {
+        print("Enter one of the following:")
+        for (item in optionsParam) print(" $item")
+        println(".")
+        val userInput = readLine()
+        if(userInput != null && userInput in optionsParam) {
+            isValidChoice = true
+            userChoice = userInput
         }
-
-        return userChoice
+        if(!isValidChoice) {
+            println("You must enter a valid choice!")
+        }
     }
 
-    fun printResult(userChoice:String, gameChoice:String) {
-        val result: String
-        println("Game Choice is $gameChoice")
-        if(userChoice.equals(gameChoice)) result = "Draw"
-        else if ((userChoice=="Rock" && gameChoice=="Scissors")||
-            (userChoice=="Scissors" && gameChoice=="Paper")||
+    return userChoice
+}
+
+fun printResult(userChoice:String, gameChoice:String) {
+    val result: String
+    println("Game Choice is $gameChoice")
+    if(userChoice == gameChoice) result = "Draw"
+    else if ((userChoice=="Rock" && gameChoice=="Scissors")||
+        (userChoice=="Scissors" && gameChoice=="Paper")||
         (userChoice=="Paper" && gameChoice=="Rock"))
         result = "You Win!"
 
-        else result = "You Lose!"
+    else result = "You Lose!"
 
-        println(result)
-    }
+    println(result)
+}
